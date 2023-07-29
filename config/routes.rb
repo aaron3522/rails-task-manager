889ec76700b085a1262f4d2c9ecd951dbecbd285
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <aaron3522@hotmail.com>
2023-07-29 16:42:09 +0100

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get '/tasks', to: 'tasks#index'
+  get 'new', to: 'tasks#new', as: :new
+  get 'tasks/:id', to: 'tasks#show', as: :task
 
   # Defines the root path route ("/")
   # root "articles#index"
---
+++ db/migrate/20230729112207_add_default_to_completed.rb
@@ -0,0 +1,6 @@
+class AddDefaultToCompleted < ActiveRecord::Migration[7.0]
+  def change
+    change_column :tasks, :completed, :boolean, default: :false
+
+  end
+end
